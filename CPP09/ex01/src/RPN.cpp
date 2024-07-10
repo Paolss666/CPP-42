d1e619af5b0bf6f15c307fd01bc1be2f6538cbab
Paolss666 <nicola.paoletti@outlook.fr>
2024-07-10 13:17:50 +0200

    cpp09 ex01

--- CPP09/ex01/src/RPN.cpp
+++ CPP09/ex01/src/RPN.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npaolett <npaolett@student.42.fr>          +#+  +:+       +#+        */
+/*   By: npoalett <npoalett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 16:52:25 by npaolett          #+#    #+#             */
-/*   Updated: 2024/07/09 18:24:10 by npaolett         ###   ########.fr       */
+/*   Updated: 2024/07/10 00:24:19 by npoalett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,42 @@ void    RPN::checkAndDo(std::string const &inpt)
     while (is >> value)
     {
         if (std::isdigit(value[0]) && value.length() == 1)
-            _stack.push(std::atoi(value.c_str()));
+            this->_stack.push(std::atoi(value.c_str()));
+        else if ((value == "+" || value == "-" || value == "/" || value == "*") && this->_stack.size() > 1)
+        {
+            vl2 = this->_stack.top();
+            this->_stack.pop();
+            vl1 = this->_stack.top();
+            this->_stack.pop();
+            if (value == "+" && (vl1 + vl2 <= 2147483647))
+                this->_stack.push(vl1 + vl2);
+            else if (value == "-" && (vl1 - vl2 >= -2147483648))
+                this->_stack.push(vl1 - vl2);
+            else if (value == "*" && (vl1 * vl2 >= -2147483648) && (vl1 * vl2 <= 2147483647))
+                this->_stack.push(vl1 * vl2);
+            else if (value == "/")
+			{
+				if (vl2 == 0)
+				{
+					std::cout << "Error : an operation is a division by 0." << std::endl;
+					return ;
+				}
+				else
+					this->_stack.push(vl1 / vl2);
+			}
+			else
+			{
+				std::cout << "Error : an operation exceeds int limits." << std::endl;
+				return ;
+			}
+        }
+        else
+            return (std::cout << "Error : bad input." << std::endl, static_cast<void>(0));
     }
+    if (this->_stack.size() != 1)
+        std::cout << "Error: bad input." << std::endl;
+    else
+        std::cout << this->_stack.top() << std::endl;
 }
 
 RPN::~RPN()
--- CPP09/ex01/src/main.cpp
+++ CPP09/ex01/src/main.cpp
@@ -1,37 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: npoalett <npoalett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/09 16:46:29 by npaolett          #+#    #+#             */
-/*   Updated: 2024/07/10 00:21:21 by npoalett         ###   ########.fr       */
+/*   Created: 2024/07/10 00:18:04 by npoalett          #+#    #+#             */
+/*   Updated: 2024/07/10 00:18:27 by npoalett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RPN_HPP
-#define RPN_HPP
+#include "../incl/RPN.hpp"
 
-# include <iostream>
-# include <stack>
-# include <string>
-# include <sstream>
-# include <cctype>
-# include <cstdlib>
-
-class RPN
+int	main(int argc, char **argv)
 {
-    private:
-        std::stack<int> _stack;
-    public:
-        RPN();
-        RPN(RPN const &src);
-        RPN &operator=(RPN const &src);
-        void    checkAndDo(std::string const &inpt);
-        ~RPN();
-};
+	if (argc == 2)
+	{
+		RPN	rpn;
 
-
-
-#endif
+		std::string input(argv[1]);
+		rpn.checkAndDo(input.c_str());
+	}
+	else
+		std::cout << "Error : invalid number of arguments" << std::endl;
+	return (0);
+}
